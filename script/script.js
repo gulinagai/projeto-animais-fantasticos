326ddb1a434635ab03db3db7b9940027a2de3b96
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-11-19 12:49:13 -0300

    projeto animais do polo norte

--- script/script.js
+++ script/script.js
@@ -1,24 +1,21 @@
-const animais = window.document.getElementById('animais')
+// Duplique o menu e adicione ele em copy
+const menu = document.querySelector('.menu')
 
+const copiamenu = menu.cloneNode(true)
 
-const primeiraLi = window.document.querySelector('li')
-console.log(primeiraLi)
+const copy = document.querySelector('.copy')
+const pcopy = document.querySelector('.copy p')
+copy.insertBefore(copiamenu, pcopy)
+// Selecione o primeiro DT do dl de Faq
 
-const primeiroUl = window.document.querySelector('ul')
-console.log(primeiroUl)
+const faq = document.querySelector('.faq')
+const primeirodt = faq.children[1].children[0]
+console.log(primeirodt)
+// Selecione o DD referente ao primeiro DT
+const ddproximodoprimeirodt = primeirodt.nextElementSibling
+console.log(ddproximodoprimeirodt)
 
-const linkInterno = window.document.querySelector('[href^="#"]')
-console.log(linkInterno)
+// Substitua o conteúdo html de .faq pelo conteúdo html de .animais
 
-const animaisImg = window.document.querySelectorAll('.animaislista img')
-console.log(animaisImg[3])
-
-const menucl = window.document.querySelector('.menu')
-
-const gridsectionnodelist = window.document.querySelectorAll('.gridsection')
-const gridsectionHTMLcol = window.document.getElementsByClassName('gridsection')
-
-menucl.classList.add('gridsection')
-
-console.log(gridsectionnodelist)
-console.log(gridsectionHTMLcol)
+const animais = document.querySelector('.animais')
+faq.innerHTML = animais.innerHTML
